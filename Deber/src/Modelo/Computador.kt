b7c986f0d406e18aac5b2184393383810cda7031
Karen <41924971+kareng9@users.noreply.github.com>
2019-05-10 12:08:08 -0500

    deber

---
+++ Deber/src/Modelo/Computador.kt
@@ -0,0 +1,11 @@
+package modelo
+
+class Computador {
+
+    var codigoComputador: Int = 0
+    var marcaComputador: String = ""
+    var tipoComputador: String = ""
+    var procesadorComputador: String = ""
+    var precioComputador: Double = 0.0
+
+}
--- README.md
+++
@@ -1,6 +0,0 @@
-# morocho-caiza-karen-gabriela
-cambio 1
-Cambio Pepito 1
-Cambio Pepito 2
-cambio Pepito 3
-Cambio Pepito 4
