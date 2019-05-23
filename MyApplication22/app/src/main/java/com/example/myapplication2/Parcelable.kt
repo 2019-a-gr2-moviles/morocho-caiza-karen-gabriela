8eff65f16d14ac8804967d22b0367f307eab20de
Karen <41924971+kareng9@users.noreply.github.com>
2019-05-23 15:25:24 -0500

    manejo parcelable

--- MyApplication22/app/src/main/java/com/example/myapplication2/Parcelable.kt
+++ MyApplication22/app/src/main/java/com/example/myapplication2/Parcelable.kt
@@ -1,12 +1,28 @@
 package com.example.myapplication2
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class Parcelable : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_parcelable)
+
+        val karen:Usuario? = this.intent.getParcelableExtra<Usuario>("usuario")
+        Log.i("parcelable", "Nombre ${karen?.nombre}")
+        Log.i("parcelable", "Nombre ${karen?.edad}")
+        Log.i("parcelable", "Nombre ${karen?.fechaNacimiento.toString()}")
+        Log.i("parcelable", "Nombre ${karen?.sueldo}")
+    }
+
+    fun regresarMenu(){
+        val intentExplicito = Intent(
+            this,
+            MainActivity::class.java
+        )
+        startActivity(intentExplicito)
     }
 }
