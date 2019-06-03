784b1dfb3aca60eda5c7ade6999cdc8bc806ff21
Karen <41924971+kareng9@users.noreply.github.com>
2019-06-02 23:57:46 -0500

    Examen Bimestral

---
+++ MorochoKaren_Examen1B/Examen/app/src/main/java/com/example/kgmc/examen/ActualizarJugadorActivity.kt
@@ -0,0 +1,70 @@
+package com.example.kgmc.examen
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_actualizar_jugador.*
+
+class ActualizarJugadorActivity : AppCompatActivity() {
+    var id :Int = 0;
+    var idPadre :Int = 0
+    var usuario :String = "";
+    var equipoRespaldo : EquipoFutbol? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actualizar_jugador)
+        usuario = intent.getStringExtra("usuario").toString()
+        val jugadorRecibido = intent.getParcelableExtra<Jugador>("Jugador")
+        equipoRespaldo = intent.getParcelableExtra<EquipoFutbol>("EquipoRespaldo")
+        txtnumeroCamiseta.setText(jugadorRecibido.numeroCamiseta.toString())
+        txtNombreCamiseta.setText(jugadorRecibido.nombreCamiseta.toString())
+        txtNombreJugador.setText(jugadorRecibido.nombreCompletoJugador.toString())
+        txtpoderEspecialDos.setText(jugadorRecibido.poderEspecialDos.toString())
+        txtfechaIngresoEquipoAct.setText(jugadorRecibido.fechaIngresoEquipo.toString())
+        txtGoles.setText(jugadorRecibido.goles.toString())
+        id = jugadorRecibido.id.toString().toInt()
+        idPadre = jugadorRecibido.equipoFutbolId.toString().toInt()
+        btnActualizarJugador.setOnClickListener { actualizarJugador() }
+        btnEliminarJugador.setOnClickListener { eliminarJugador() }
+    }
+
+    fun actualizarJugador(){
+        val jugador = Jugador(
+            id = id,
+            numeroCamiseta = txtnumeroCamiseta.text.toString().toInt(),
+            nombreCamiseta = txtNombreCamiseta.text.toString(),
+            nombreCompletoJugador = txtNombreJugador.text.toString(),
+            poderEspecialDos = txtpoderEspecialDos.text.toString(),
+            fechaIngresoEquipo = txtfechaIngresoEquipoAct.text.toString(),
+            goles = txtGoles.text.toString().toInt(),
+            equipoFutbolId = idPadre
+        )
+        Jugador_BD.actualizarJugador(jugador)
+        Toast.makeText(this, getString(R.string.msgActualizar)+" "+usuario, Toast.LENGTH_SHORT).show()
+        val retorno = Intent(this, ConsultarJugadorActivity::class.java)
+        retorno.putExtra("usuario", usuario)
+        retorno.putExtra("padreId", idPadre)
+        retorno.putExtra("EquipoRespaldo", equipoRespaldo)
+        startActivity(retorno)
+    }
+
+    fun eliminarJugador(){
+        Jugador_BD.eliminarJugador(id)
+        Toast.makeText(this, getString(R.string.msgEliminar)+" "+usuario, Toast.LENGTH_SHORT).show()
+        val retorno = Intent(this, ConsultarJugadorActivity::class.java)
+        retorno.putExtra("usuario", usuario)
+        retorno.putExtra("padreId", idPadre)
+        retorno.putExtra("EquipoRespaldo", equipoRespaldo)
+        startActivity(retorno)
+    }
+    override fun onBackPressed() {
+
+
+        val intentMenu = Intent(this, ConsultarJugadorActivity::class.java)
+        intentMenu.putExtra("usuario", usuario)
+        intentMenu.putExtra("padreId", idPadre)
+        intentMenu.putExtra("EquipoRespaldo", equipoRespaldo)
+        startActivity(intentMenu)
+    }
+}
---
+++ MorochoKaren_Examen1B/Examen/app/src/main/java/com/example/kgmc/examen/ConsultarJugadorActivity.kt
@@ -0,0 +1,46 @@
+package com.example.kgmc.examen
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_consultar_jugador.*
+
+class ConsultarJugadorActivity : AppCompatActivity() {
+    var padreId : Int = 0
+    var usuario :String = "";
+    var equipoRespaldo : EquipoFutbol? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        usuario = intent.getStringExtra("usuario").toString()
+        equipoRespaldo = intent.getParcelableExtra<EquipoFutbol>("EquipoRespaldo")
+        padreId = intent.getIntExtra("padreId", -1)
+        val equipo=intent.getParcelableExtra<EquipoFutbol>("EquipoRespaldo")
+        setContentView(R.layout.activity_consultar_jugador)
+        val nombre=equipo.nombre.toString();
+        val adapter = ArrayAdapter<Jugador>(
+            this,
+            android.R.layout.simple_list_item_1,
+            Jugador_BD.mostrarJugador(padreId)
+        )
+        lstJugador.adapter = adapter;
+        lstJugador.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+
+            val jugadorSeleccionado = parent.getItemAtPosition(position) as Jugador
+            val intentJugadorSeleccionado = Intent(this, ActualizarJugadorActivity::class.java)
+            intentJugadorSeleccionado.putExtra("usuario", usuario)
+            intentJugadorSeleccionado.putExtra("Jugador", jugadorSeleccionado)
+            intentJugadorSeleccionado.putExtra("EquipoRespaldo", equipoRespaldo)
+            startActivity(intentJugadorSeleccionado)
+        }
+        textView12.setText(getString(R.string.nombreJugador)+": "+nombre)
+    }
+    override fun onBackPressed() {
+        val intentMenu = Intent(this, ActualizarActivity::class.java)
+        intentMenu.putExtra("Equipo", equipoRespaldo)
+        intentMenu.putExtra("usuario", usuario)
+        startActivity(intentMenu)
+    }
+}
