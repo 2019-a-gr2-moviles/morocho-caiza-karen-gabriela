16816d7abd0929551b89204bb65dc238a29dcb69
Karen <41924971+kareng9@users.noreply.github.com>
2019-07-11 16:18:46 -0500

    Maps - Poligonos - Lineas

--- 03-Android/app/src/main/java/com/example/myapplication/MapsActivity.kt
+++ 03-Android/app/src/main/java/com/example/myapplication/MapsActivity.kt
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.widget.DialogTitle
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
@@ -12,10 +13,33 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(),
+    OnMapReadyCallback,
+        GoogleMap.OnCameraMoveStartedListener,
+        GoogleMap.OnCameraMoveListener,
+        GoogleMap.OnCameraIdleListener,
+        GoogleMap.OnPolylineClickListener,
+        GoogleMap.OnPolygonClickListener
+{
+    override fun onPolygonClick(p0: Polygon?) {
+        Log.i("mapa", "Polygone ${p0.toString()}")
+    }
+
+    override fun onPolylineClick(p0: Polyline?) {
+       Log.i("mapa","Polyline ${p0.toString()}") }
+
+    override fun onCameraIdle() {
+        Log.i("map", "Me quede quieto")
+    }
+
+    override fun onCameraMove() {
+        Log.i("map", "Me estoy moviendo")     }
+
+    override fun onCameraMoveStarted(p0: Int) {
+        Log.i("map", "Me voy a empezar a mover")
+    }
 
     private lateinit var mMap: GoogleMap
     private var tienePermisosLocalizacion = false
@@ -46,16 +70,70 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         establecerConfiguracionMapa(mMap)
+        establecerListenersMovimientoMapa(mMap)
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-0.210047, -78.488574)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,17f))
+        // Add a marker in Sydney and move the camera (cambiamos a Plaza Foch)
+        val foch = LatLng(-0.202760, -78.490813)
+        val titulo = "Plaza Foch"
+        val zoom = 17f
+        anadirMarcador(foch, titulo)
+        moverCamaraConZomm(foch,zoom)
+
+        val poliLineaUno = googleMap
+            .addPolyline(PolylineOptions().clickable(true)
+                .add(
+                LatLng(-0.210462, -78.493948),
+                LatLng(-0.208218, -78.490163),
+                LatLng(-0.208583, -78.488940),
+                LatLng(-0.209377, -78.490303)
+            ))
+        val poligonoUno = googleMap
+            .addPolygon(PolygonOptions().clickable(true)
+                .add(
+                    LatLng(-0.209431, -78.490078),
+                    LatLng(-0.208734, -78.488951),
+                    LatLng(-0.209431, -78.488286),
+                    LatLng(-0.210085, -78.489745)
+                ))
+        poligonoUno.fillColor = -0XC711C4
+
+    }
+
+    fun establecerListenersMovimientoMapa(map: GoogleMap){
+        with(map){
+            setOnCameraIdleListener(this@MapsActivity)
+            setOnCameraMoveStartedListener(this@MapsActivity)
+            setOnCameraMoveListener(this@MapsActivity)
+            setOnPolylineClickListener(this@MapsActivity)
+            setOnPolygonClickListener (this@MapsActivity)
+
+        }
+    }
+
+    fun anadirMarcador(latLng: LatLng, title: String){
+        mMap.addMarker(
+            MarkerOptions().position(latLng)
+                .title(title)
+        )
+
+    }
+
+    fun moverCamaraConZomm(latLng: LatLng, zoom:Float = 10f){
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom))
     }
 
     fun establecerConfiguracionMapa(mapa:GoogleMap){
+        val contexto = this.applicationContext
         with(mapa){
-            mapa.isMyLocationEnabled = true
+            val permisoFineLocation = ContextCompat
+                .checkSelfPermission(
+                    contexto,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            val tienePermiso = permisoFineLocation == PackageManager.PERMISSION_GRANTED
+            if(tienePermiso) {
+                mapa.isMyLocationEnabled = true
+            }
             this.uiSettings.isZoomControlsEnabled = true
             uiSettings.isMyLocationButtonEnabled = true
         }
@@ -84,4 +162,5 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
 
     }
+
 }
